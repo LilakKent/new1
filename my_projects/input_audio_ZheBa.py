184d74c49a46ef190a500609184d7968f89c1fc2
Lucky_ZheBa <81864843+LilakKent@users.noreply.github.com>
2023-11-13 14:50:04 +0100

    input_audio_ZheBa.py
    input, that u say

---
+++ my_projects/input_audio_ZheBa.py
@@ -0,0 +1,18 @@
+import json, pyaudio
+from vosk import Model, KaldiRecognizer
+
+def listen():
+    model_ru = Model('models/vosk_model')
+    rec = KaldiRecognizer(model_ru, 16000)
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+    stream.start_stream()
+
+
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+        if (rec.AcceptWaveform(data)) and (len(data) > 0):
+            answer = json.loads(rec.Result())
+            if answer['text']:
+
+                return answer['text']
